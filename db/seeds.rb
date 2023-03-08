b9196854e277df84d5a59814359436b7d1bcc805
cristian fuhrich <fuehrich@gmail.com>
2023-03-08 10:34:59 +0000

    seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,8 +5,30 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
-# Create some test users
+
+
 User.create(email: 'user1@example.com', password: 'password')
 User.create(email: 'user2@example.com', password: 'password')
 User.create(email: 'user3@example.com', password: 'password')
 
+GameSession.create(
+  avatar_url: 'https://www.w3schools.com/howto/img_avatar2.png',
+  game_id: 1,
+  user_id: 1
+)
+
+GameSession.create(
+  avatar_url: 'https://www.w3schools.com/w3images/avatar5.png',
+  game_id: 2,
+  user_id: 2
+)
+
+GameSession.create(
+  avatar_url: 'https://www.w3schools.com/w3images/avatar6.png',
+  game_id: 3,
+  user_id: 3
+)
+
+# game creation
+# game = Game.create(room_code: "ABC123", user_id: 1)
+# game.save
