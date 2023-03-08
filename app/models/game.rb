fab4a4407f406b8905069c9ab4dc3cedc09471af
Janina Lodeiro <nina.lodeiro@gmail.com>
2023-03-08 12:46:00 +0000

    homepage buttons created and games routes as well

--- app/models/game.rb
+++ app/models/game.rb
@@ -1,3 +1,8 @@
 class Game < ApplicationRecord
   belongs_to :user
+  # enum kind: [ :quandary ]
+  validates_presence_of :room
+  validates_presence_of :min_players
+  validates_presence_of :max_players
+  validates :min_players, numericality: { less_than_or_equal_to: :max_players, message: "must be less than, or equal to, max players!"}
 end
