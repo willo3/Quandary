92a4c288ed45e00e1a31f3d1283cbafeaaa0628d
William Parker <wrparker@gmail.com>
2023-03-08 15:33:55 +0000

    Fixed seed conflict

--- app/models/game.rb
+++ app/models/game.rb
@@ -3,8 +3,8 @@ class Game < ApplicationRecord
   has_many :game_sessions, dependent: :destroy
   has_many :dilemmas, dependent: :destroy
   # enum kind: [ :quandary ]
-  validates_presence_of :room
-  validates_presence_of :min_players
-  validates_presence_of :max_players
-  validates :min_players, numericality: { less_than_or_equal_to: :max_players, message: "must be less than, or equal to, max players!"}
+  validates_presence_of :room_code
+  # validates_presence_of :min_players
+  # validates_presence_of :max_players
+  # validates :min_players, numericality: { less_than_or_equal_to: :max_players, message: "must be less than, or equal to, max players!"}
 end
