4d86a7c0815281cbbed4df2b409761165678a305
William Parker <wrparker@gmail.com>
2023-03-10 04:44:26 +0000

    Aaaaah

--- app/controllers/results_controller.rb
+++ app/controllers/results_controller.rb
@@ -1,5 +1,21 @@
 class ResultsController < ApplicationController
   def index
-    
+    # raise
+    @dilemma = Dilemma.find(params[:dilemma_id])
+    @result_a = Result.where(dilemma: @dilemma).and(Result.where(scenario: @dilemma.scenarios.first))
+    @result_b = Result.where(dilemma: @dilemma).and(Result.where(scenario: @dilemma.scenarios.second))
+  end
+
+  def create
+    @user = current_user
+    @dilemma = Dilemma.find(params[:dilemma_id])
+    @scenario = Scenario.find(params[:scenario_id])
+    @result = Result.new(user: @user, dilemma: @dilemma, scenario: @scenario)
+    @game = Game.find(params[:game_id])
+    if @result.save!
+      redirect_to game_dilemma_results_path(@game, @dilemma)
+    else
+      redirect_to game_dilemma_path(@game, @dilemma)
+    end
   end
 end
