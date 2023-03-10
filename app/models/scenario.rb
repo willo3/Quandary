eb92b612be64516bdf44105e74a5bbe8256b99f3
cristian fuhrich <fuehrich@gmail.com>
2023-03-10 14:55:51 +0000

    results controller

--- app/models/scenario.rb
+++ app/models/scenario.rb
@@ -2,4 +2,8 @@ class Scenario < ApplicationRecord
   # belongs_to :dilemma
   has_many :dilemma_scenarios
   has_many :dilemmas, through: :dilemma_scenarios
+
+  # added from creation results controler
+  has_many :results
+
 end
