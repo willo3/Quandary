3bc3205dfa1367b0bfbaeabb7e0cf5767f85a0c0
Janina Lodeiro <nina.lodeiro@gmail.com>
2023-03-13 15:17:13 +0000

    join game done

---
+++ app/controllers/players_controller.rb
@@ -0,0 +1,16 @@
+class PlayersController < ApplicationController
+  # def create
+  #   @player = Player.new(avatar_url: Player::AVATARS.sample)
+  #   @player.game = @game
+  #   @player.user = current_user
+  #   if @player.save
+  #     GameChannel.broadcast_to(
+  #       @game,
+  #       render_to_string(partial: "players/player", locals: {player: @player})
+  #     )
+  #     head :ok
+  #   else
+  #     render :index, status: :unprocessable_entity
+  #   end
+  # end
+end
