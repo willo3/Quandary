135e59b2f2be88967b0d75d1dcb62d9bdb930e71
William Parker <wrparker@gmail.com>
2023-03-16 20:08:40 +0000

    Found the avatar causing the random crash

--- app/models/player.rb
+++ app/models/player.rb
@@ -6,7 +6,7 @@ class Player < ApplicationRecord
               'https://res.cloudinary.com/drfmymoki/image/upload/v1678354414/Quandary%20Avatars/Avatar_Cat_2_f4sv5p.svg',
               'https://res.cloudinary.com/drfmymoki/image/upload/v1678354431/Quandary%20Avatars/Avatar_Froggo_1_vbq7r2.svg',
               'https://res.cloudinary.com/drfmymoki/image/upload/v1678354414/Quandary%20Avatars/Avatar_Froggo_2_ddynz0.svg',
-              'https://res.cloudinary.com/drfmymoki/image/upload/v1678796762/Quandary%20Avatars/Avatar_Imp_dbc3ns.svg',
+              # 'https://res.cloudinary.com/drfmymoki/image/upload/v1678796762/Quandary%20Avatars/Avatar_Imp_dbc3ns.svg',
               'https://res.cloudinary.com/drfmymoki/image/upload/v1678797373/Quandary%20Avatars/Avatar_Shiba_heoqqe.svg',
               'https://res.cloudinary.com/drfmymoki/image/upload/v1678797110/Quandary%20Avatars/Avatar_Froggo_3_pehjhg.svg',
               'https://res.cloudinary.com/drfmymoki/image/upload/v1678798232/Quandary%20Avatars/Avatar_Bear_1_love_crzgrb.svg',
